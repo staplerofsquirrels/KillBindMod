f0aafab5e394627eb7e9f7c2bf4bfc9f4367ec03
staplerofsquirrels <electronica.bypmaster@gmail.com>
2024-08-27 19:08:43 +0200

    Add files via upload

---
+++ mainclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using MelonLoader;
+using HarmonyLib;
+namespace KillBind
+{
+    public class mainclass :MelonMod
+    {
+        public override void OnUpdate()
+        {
+            if(GameObject.Find("Player") != null && Input.GetKeyDown(KeyCode.K))
+            {
+                PlayerHealthManagement phmscript = GameObject.Find("Player").GetComponent<PlayerHealthManagement>();
+                phmscript.TakeDamage(phmscript.MyHealth * 6f);
+            }
+        }
+    }
+}
